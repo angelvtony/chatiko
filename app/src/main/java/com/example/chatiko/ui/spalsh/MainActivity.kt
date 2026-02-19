d3af1cd68c5c3a78cebd5b96f75b1e66a40f7ef7
angel.tony <angel.tony@acabes.com>
2026-02-19 10:38:04 +0530

    init

---
+++ app/src/main/java/com/example/chatiko/ui/spalsh/MainActivity.kt
@@ -0,0 +1,144 @@
+package com.example.chatiko.ui.spalsh
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.animateColor
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import com.example.chatiko.ui.theme.ChatikoTheme
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.LocationOn
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.animation.core.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.graphicsLayer
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ChatikoTheme {
+                SplashScreen()
+            }
+        }
+    }
+}
+
+@Composable
+fun SplashScreen() {
+    val transition = rememberInfiniteTransition()
+
+    val scale by transition.animateFloat(
+        initialValue = 0.8f,
+        targetValue = 1.2f,
+        animationSpec = infiniteRepeatable(tween(durationMillis = 1000, easing = FastOutSlowInEasing))
+    )
+
+    val rotation by transition.animateFloat(
+        initialValue = 0f,
+        targetValue = 360f,
+        animationSpec = infiniteRepeatable(tween(durationMillis = 2000, easing = LinearEasing))
+    )
+
+    val translationY by transition.animateFloat(
+        initialValue = 0f,
+        targetValue = 20f,
+        animationSpec = infiniteRepeatable(tween(durationMillis = 1000, easing = FastOutSlowInEasing))
+    )
+
+    val iconColor by transition.animateColor(
+        initialValue = Color(0xFFFF0000),
+        targetValue = Color(0xFFFF0000),
+        animationSpec = infiniteRepeatable(tween(durationMillis = 1000, easing = FastOutSlowInEasing))
+    )
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                brush = Brush.linearGradient(
+                    colors = listOf(
+                        Color(0xFF00C6FF),
+                        Color(0xFFEAC89B),
+                    ),
+                    start = Offset(0f, 0f),
+                )
+            )
+            .padding(24.dp)
+    ) {
+
+        Column(
+            modifier = Modifier.align(Alignment.Center),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            Box(
+                modifier = Modifier
+                    .size(120.dp)
+                    .shadow(20.dp, CircleShape)
+                    .background(Color.White, CircleShape)
+                    .scale(scale)
+                    .graphicsLayer(
+                        rotationZ = rotation,
+                        translationY = translationY
+                    ),
+                contentAlignment = Alignment.Center
+            ) {
+                Icon(
+                    imageVector = Icons.Default.LocationOn,
+                    contentDescription = "Logo",
+                    tint = iconColor,
+                    modifier = Modifier.size(60.dp)
+                )
+            }
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+            Text(
+                text = "NearYou",
+                style = MaterialTheme.typography.headlineLarge,
+                fontWeight = FontWeight.Bold,
+                color = Color.White
+            )
+        }
+
+        Text(
+            text = "Anonymous Nearby Mood Social Media",
+            style = MaterialTheme.typography.bodyMedium,
+            color = Color.Gray.copy(alpha = 0.85f),
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(bottom = 32.dp)
+        )
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun SplashPreview() {
+    ChatikoTheme {
+        SplashScreen()
+    }
+}
+
